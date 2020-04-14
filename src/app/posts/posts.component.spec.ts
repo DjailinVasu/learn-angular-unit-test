f3aa845b1e7584c41cf8cc69f42b1f348948e8e5
Mary Denysova <djailin.vasu@gmail.com>
2020-04-14 21:03:04 +0300

    Тестирование confirm метода

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -54,8 +54,24 @@ describe('PostsComponent', () => {
       expect(component.message).toBe(error);
   })
 
-  // it('', () => {
+  it('should remove post if user confirms', () => {
+     const spy =  spyOn(service, 'remove').and.returnValue(EMPTY);
 
-  // })
+    spyOn(window, 'confirm').and.returnValue(true);
+
+    component.delete(10);
+
+    expect(spy).toHaveBeenCalledWith(10);
+  })
+
+  it('should NOT remove post if user doesnt confirms', () => {
+    const spy =  spyOn(service, 'remove').and.returnValue(EMPTY);
+
+   spyOn(window, 'confirm').and.returnValue(false);
+
+   component.delete(10);
+
+   expect(spy).not.toHaveBeenCalled();
+ })
 
 })
